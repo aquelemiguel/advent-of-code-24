1625b4eb4637651ff3ee467add57cf8eb2abb198
Miguel Mano <aquelemiguel@gmail.com>
2024-12-04 18:49:04 +0000

    🎁

--- solutions/d04.ts
+++ solutions/d04.ts
@@ -1,9 +1,8 @@
+import * as _ from "jsr:@es-toolkit/es-toolkit";
+
 function parse(input: string): string[][] {
-  return [
-    [],
-    ...input.split("\n").map((line) => ["", ...line.split(""), ""]),
-    []
-  ];
+  const arr = input.split("\n").map((line) => ["", ...line.split(""), ""]);
+  return [Array(arr.length).fill(""), ...arr, Array(arr.length).fill("")];
 }
 
 function search(
@@ -12,44 +11,60 @@ function search(
   x: number,
   y: number,
   stage: number,
-  [dx, dy]: number[]
-): boolean {
-  return (
-    stage >= word.length ||
-    (soup[x + dx][y + dy] === word[stage] &&
-      search(soup, word, x + dx, y + dy, stage + 1, [dx, dy]))
-  );
+  [dx, dy]: number[],
+  visited = [[x, y]]
+): number[][] {
+  if (visited.length === word.length) {
+    return visited;
+  }
+  if (soup[x + dx][y + dy] === word[stage]) {
+    return search(
+      soup,
+      word,
+      x + dx,
+      y + dy,
+      stage + 1,
+      [dx, dy],
+      [...visited, [x + dx, y + dy]]
+    );
+  }
+  return [];
 }
 
 export function p1(input: string): number {
-  const neighbours = [-1, 0, 1].flatMap((dx) =>
+  const pattern = [-1, 0, 1].flatMap((dx) =>
     [-1, 0, 1].map((dy) => [dx, dy]).filter(([dx, dy]) => dx !== 0 || dy !== 0)
   );
   const soup = parse(input);
-  return soup
-    .flatMap((row, i) =>
-      row.flatMap((cell, j) =>
-        cell === "X"
-          ? neighbours.filter(([dx, dy]) =>
-              search(soup, "XMAS", i, j, 1, [dx, dy])
-            ).length
-          : 0
-      )
+  return soup.flatMap((row, i) =>
+    row.flatMap((cell, j) =>
+      cell === "X"
+        ? pattern
+            .map((dir) => search(soup, "XMAS", i, j, 1, dir))
+            .filter((res) => res.length !== 0)
+        : []
     )
-    .reduce((acc, val) => acc + val, 0);
+  ).length;
 }
 
 export function p2(input: string): number {
-  const isDiagonal = (edges: [string?, string?]): boolean =>
-    ["MS", "SM"].includes(edges.join(""));
-
+  const pattern = [
+    [-1, -1],
+    [-1, 1],
+    [1, -1],
+    [1, 1]
+  ];
   const soup = parse(input);
-  return soup.flatMap((row, i) =>
-    row.filter(
-      (cell, j) =>
-        cell === "A" &&
-        isDiagonal([soup[i - 1][j - 1], soup[i + 1][j + 1]]) &&
-        isDiagonal([soup[i - 1][j + 1], soup[i + 1][j - 1]])
+  const res = soup.flatMap((row, i) =>
+    row.flatMap((cell, j) =>
+      cell === "M"
+        ? pattern
+            .map((dir) => search(soup, "MAS", i, j, 1, dir))
+            .filter((res) => res.length !== 0)
+        : []
     )
+  );
+  return Object.values(_.countBy(res, (r) => r[1].join(" "))).filter(
+    (v) => v === 2
   ).length;
 }
