998da047704f03be6dc72b40b3a6e61cdb99e3df
Miguel Mano <aquelemiguel@gmail.com>
2024-12-07 18:16:04 +0000

    ⛄

--- solutions/d07/d07.ts
+++ solutions/d07/d07.ts
@@ -1,4 +1,8 @@
-import * as _ from "jsr:@es-toolkit/es-toolkit";
+const opMap: any = {
+  "+": (a: number, b: number) => a + b,
+  "*": (a: number, b: number) => a * b,
+  "||": (a: number, b: number) => Number(`${a}${b}`),
+};
 
 function parse(input: string) {
   return input
@@ -21,45 +25,28 @@ function permutate(chars: string[], n: number): string[][] {
   return perms;
 }
 
-function combine(result: number, operands: number[], operators: string[]) {
-  const perms = permutate(operators, operands.length - 1);
-  return perms.filter(
+function combine(result: number, oprs: number[], ops: string[]) {
+  return permutate(ops, oprs.length - 1).filter(
     (perm) =>
-      perm.reduce((acc, op, i) => {
-        if (op === "+") {
-          return acc + operands[i + 1];
-        }
-        if (op === "*") {
-          return acc * operands[i + 1];
-        }
-        if (op === "||") {
-          const bDigits = Math.floor(Math.log10(operands[i + 1])) + 1;
-          return acc * Math.pow(10, bDigits) + operands[i + 1];
-        }
-        return acc;
-      }, operands[0]) === result
+      perm.reduce((acc, op, i) => opMap[op](acc, oprs[i + 1]), oprs[0]) ===
+      result
   );
 }
 
-function calibrate(equations: number[][], operators: string[]) {
-  let sum = 0;
-  for (const [result, ...operands] of equations) {
-    const perms = combine(result, operands, operators);
-    if (perms.length > 0) {
-      sum += result;
-    }
-  }
-  return sum;
+function calibrate(eqs: number[][], ops: string[]) {
+  return eqs.reduce((acc, [res, ...oprs]) => {
+    return combine(res, oprs, ops).length > 0 ? acc + res : acc;
+  }, 0);
 }
 
 function p1(input: string) {
-  const equations = parse(input);
-  return calibrate(equations, ["+", "*"]);
+  const eqs = parse(input);
+  return calibrate(eqs, ["+", "*"]);
 }
 
 function p2(input: string) {
-  const equations = parse(input);
-  return calibrate(equations, ["+", "*", "||"]);
+  const eqs = parse(input);
+  return calibrate(eqs, ["+", "*", "||"]);
 }
 
 export { p1, p2 };
