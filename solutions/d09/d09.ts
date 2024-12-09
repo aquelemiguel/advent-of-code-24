8af6b557d52f9e3f4e94bbee134652a17df3eca3
Miguel Mano <aquelemiguel@gmail.com>
2024-12-09 22:55:09 +0000

    🎄

--- solutions/d09/d09.ts
+++ solutions/d09/d09.ts
@@ -1,75 +1,58 @@
 function parse(input: string) {
-  const blocks: number[] = [];
-  for (let i = 0; i < input.length; i++) {
-    blocks.push(
-      ...new Array(Number(input[i])).fill(
-        i % 2 === 0 ? Math.max(0, i / 2) : NaN
-      )
+  return input
+    .split("")
+    .flatMap((_, i) =>
+      new Array(Number(input[i])).fill(i % 2 === 0 ? Math.max(0, i / 2) : -1)
     );
-  }
-  return blocks;
 }
 
-function fullDefrag(blocks: number[]) {
-  let startBlock = 0;
+function checksum(blocks: number[]) {
+  return blocks.reduce((acc, curr, i) => acc + (curr === -1 ? 0 : curr * i), 0);
+}
 
-  for (let j = blocks.length - 1; j >= 0; j--) {
-    if (Number.isNaN(blocks[j])) {
+function defrag(blocks: number[]) {
+  for (let j = blocks.length - 1, startBlock = 0; j >= 0; j--) {
+    if (blocks[j] === -1) {
       continue;
     }
     for (let i = startBlock; i < j; i++) {
-      if (Number.isNaN(blocks[i])) {
-        blocks[i] = blocks[j];
-        blocks[j] = NaN;
-        startBlock = i + 1;
-        break;
+      if (blocks[i] !== -1) {
+        continue;
       }
+      blocks[i] = blocks[j];
+      blocks[j] = -1;
+      startBlock = i + 1;
+      break;
     }
   }
   return blocks;
 }
 
 function quickDefrag(blocks: number[]) {
-  console.log(blocks);
   for (let j = blocks.length - 1; j >= 0; j--) {
-    if (Number.isNaN(blocks[j])) {
+    if (blocks[j] === -1) {
       continue;
     }
     const currentBlock = blocks[j];
     let blockSize = 1;
-
-    while (blocks[j - 1] === currentBlock) {
-      j--;
-      blockSize++;
+    while (j > 0 && blocks[j - 1] === currentBlock) {
+      blockSize++, j--;
     }
-
-    findFreeSlots: for (let i = 0, freeSlots = 0; i < j; ) {
-      while (Number.isNaN(blocks[i])) {
-        freeSlots++;
-        if (freeSlots === blockSize) {
-          for (let k = freeSlots; k > 0; k--, i--) {
-            blocks[i] = currentBlock;
-          }
-          for (let k = j; k < j + blockSize; k++) {
-            blocks[k] = NaN;
-          }
-          break findFreeSlots;
+    for (let i = 0, freeSlots = 0; i < j; i++) {
+      if (blocks[i] === -1 && ++freeSlots === blockSize) {
+        for (let k = 0; k < blockSize; k++) {
+          blocks[i - k] = currentBlock;
+          blocks[j + k] = -1;
         }
-        i++;
+        break;
       }
-      i++;
-      freeSlots = 0;
+      if (blocks[i] !== -1) {
+        freeSlots = 0;
+      }
     }
   }
   return blocks;
 }
 
-function checksum(blocks: number[]): number {
-  return blocks.reduce(
-    (acc, curr, i) => acc + (Number.isNaN(curr) ? 0 : curr * i),
-    0
-  );
-}
-
-export const p1 = (input: string) => checksum(fullDefrag(parse(input)));
+export const p1 = (input: string) => checksum(defrag(parse(input)));
 export const p2 = (input: string) => checksum(quickDefrag(parse(input)));
