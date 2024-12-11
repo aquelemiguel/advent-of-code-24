94e97d191e8ac9d533a016ba73118e447d0894c0
Miguel Mano <aquelemiguel@gmail.com>
2024-12-11 16:25:07 +0000

    🎄

--- solutions/d11/d11.ts
+++ solutions/d11/d11.ts
@@ -1,47 +1,40 @@
 import * as _ from "jsr:@es-toolkit/es-toolkit";
 
-class StoneList<T> {
-  map = new Map<T, number>();
-  constructor(stones: T[] = []) {
-    stones.forEach((stone) => this.add(stone, 1));
-  }
-  add(stone: T, count: number): void {
-    this.map.set(stone, (this.map.get(stone) || 0) + count);
-  }
-  count(): number {
-    return _.sum([...this.map.values()]);
-  }
+function addToMap<T>(map: Map<T, number>, key: T, count: number) {
+  map.set(key, (map.get(key) || 0) + count);
 }
 
-function parse(input: string): StoneList<number> {
-  return new StoneList(input.split(" ").map((n) => Number(n)));
+function countMapValues<T>(map: Map<T, number>) {
+  return _.sum([...map.values()]);
 }
 
-function cutString(str: string): number[] {
-  return _.chunk(str.split(""), str.length / 2).map((n: string[]) =>
-    Number(n.join(""))
-  );
+function parse(input: string) {
+  const map = new Map<number, number>();
+  input.split(" ").forEach((n) => addToMap(map, Number(n), 1));
+  return map;
 }
 
-function blink(stones: StoneList<number>, n: number): number {
+function blink(stones: Map<number, number>, n: number) {
   while (n--) {
-    const newStones = new StoneList<number>();
+    const newStones = new Map<number, number>();
 
-    for (const [stone, count] of stones.map) {
+    for (const [stone, count] of stones) {
       const str = stone.toString();
       if (stone === 0) {
-        newStones.add(1, count);
+        addToMap(newStones, 1, count);
       } else if (str.length % 2 === 0) {
-        const [left, right] = cutString(str);
-        newStones.add(left, count);
-        newStones.add(right, count);
+        const [left, right] = _.chunk(str.split(""), str.length / 2).map(
+          (n: string[]) => Number(n.join(""))
+        );
+        addToMap(newStones, left, count);
+        addToMap(newStones, right, count);
       } else {
-        newStones.add(stone * 2024, count);
+        addToMap(newStones, stone * 2024, count);
       }
     }
     stones = newStones;
   }
-  return stones.count();
+  return countMapValues(stones);
 }
 
 export const p1 = (input: string) => blink(parse(input), 25);
