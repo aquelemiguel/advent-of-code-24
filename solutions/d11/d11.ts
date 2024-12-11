a0fdca453bbfba47125d8256c5adc0f867073f7a
Miguel Mano <aquelemiguel@gmail.com>
2024-12-11 16:34:34 +0000

    🎄

--- solutions/d11/d11.ts
+++ solutions/d11/d11.ts
@@ -1,12 +1,12 @@
 import * as _ from "jsr:@es-toolkit/es-toolkit";
 
-function addToMap(map: Map<number, number>, key: number, count: number) {
+function addStone(map: Map<number, number>, key: number, count: number) {
   map.set(key, (map.get(key) || 0) + count);
 }
 
 function parse(input: string) {
   const map = new Map<number, number>();
-  input.split(" ").forEach((n) => addToMap(map, Number(n), 1));
+  input.split(" ").forEach((n) => addStone(map, Number(n), 1));
   return map;
 }
 
@@ -17,15 +17,15 @@ function blink(stones: Map<number, number>, n: number) {
     for (const [stone, count] of stones) {
       const str = stone.toString();
       if (stone === 0) {
-        addToMap(newStones, 1, count);
+        addStone(newStones, 1, count);
       } else if (str.length % 2 === 0) {
         const [left, right] = _.chunk(str.split(""), str.length / 2).map(
           (n: string[]) => Number(n.join(""))
         );
-        addToMap(newStones, left, count);
-        addToMap(newStones, right, count);
+        addStone(newStones, left, count);
+        addStone(newStones, right, count);
       } else {
-        addToMap(newStones, stone * 2024, count);
+        addStone(newStones, stone * 2024, count);
       }
     }
     stones = newStones;
