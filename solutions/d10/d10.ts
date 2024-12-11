b21912970d34797c5cbefface05522de5d25b510
Miguel Mano <aquelemiguel@gmail.com>
2024-12-11 12:04:03 +0000

    🎄

--- solutions/d10/d10.ts
+++ solutions/d10/d10.ts
@@ -1,57 +1,52 @@
 import * as _ from "jsr:@es-toolkit/es-toolkit";
 
+type Point = [number, number];
+
 function parse(input: string) {
-  return input
-    .split("\n")
-    .map((line) => line.split("").map((n) => parseInt(n)));
+  return input.split("\n").map((line) => line.split("").map(Number));
 }
 
-function _hike(
-  map: number[][],
-  [i, j]: [number, number],
-  h = 0,
-  visited: number[][] = []
-) {
-  if (map[i][j] === 9) {
-    visited.push([i, j]);
-    return visited;
-  }
-  const neighbors = [
-    [i - 1, j],
-    [i + 1, j],
-    [i, j - 1],
-    [i, j + 1]
-  ];
-  for (const [x, y] of neighbors) {
-    if (map[x]?.[y] === h + 1) {
-      _hike(map, [x, y], h + 1, visited);
-    }
-  }
-  return visited;
+function score(visited: Point[][]) {
+  return _.sumBy(
+    visited,
+    (head: Point[]) => _.uniqBy(head, ([x, y]) => `${x},${y}`).length
+  );
+}
+
+function rating(visited: Point[][]) {
+  return _.sumBy(visited, (head: Point[]) => head.length);
 }
 
 function hike(map: number[][]) {
-  const paths: number[][][] = [];
+  const paths: Point[][] = [];
+
+  function explore([i, j]: Point, h = 0, visited: Point[] = []) {
+    if (map[i][j] === 9) {
+      visited.push([i, j]);
+      return visited;
+    }
+    const neighbors = [
+      [i - 1, j],
+      [i + 1, j],
+      [i, j - 1],
+      [i, j + 1]
+    ];
+    for (const [x, y] of neighbors) {
+      if (map[x]?.[y] === h + 1) {
+        explore([x, y], h + 1, visited);
+      }
+    }
+    return visited;
+  }
 
   for (let i = 0; i < map.length; i++) {
     for (let j = 0; j < map[i].length; j++) {
       if (map[i][j] === 0) {
-        paths.push(_hike(map, [i, j]));
+        paths.push(explore([i, j]));
       }
     }
   }
   return paths;
-}
-
-function score(visited: number[][][]) {
-  return _.sumBy(
-    visited,
-    (head: number[][]) => _.uniqBy(head, ([x, y]) => `${x},${y}`).length
-  );
-}
-
-function rating(visited: number[][][]) {
-  return _.sumBy(visited, (head: number[][]) => head.length);
 }
 
 export const p1 = (input: string): number => score(hike(parse(input)));
