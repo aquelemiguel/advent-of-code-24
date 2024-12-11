4a2a7f984232ad447d0b223d707f2dbb35b32519
Miguel Mano <aquelemiguel@gmail.com>
2024-12-11 16:22:05 +0000

    🎄

--- solutions/d11/d11.ts
+++ solutions/d11/d11.ts
@@ -1,18 +1,15 @@
 import * as _ from "jsr:@es-toolkit/es-toolkit";
 
 class StoneList<T> {
-  private stones = new Map<T, number>();
+  map = new Map<T, number>();
   constructor(stones: T[] = []) {
     stones.forEach((stone) => this.add(stone, 1));
   }
   add(stone: T, count: number): void {
-    this.stones.set(stone, (this.stones.get(stone) || 0) + count);
-  }
-  getAll(): Map<T, number> {
-    return this.stones;
+    this.map.set(stone, (this.map.get(stone) || 0) + count);
   }
   count(): number {
-    return _.sum([...this.stones.values()]);
+    return _.sum([...this.map.values()]);
   }
 }
 
@@ -20,24 +17,26 @@ function parse(input: string): StoneList<number> {
   return new StoneList(input.split(" ").map((n) => Number(n)));
 }
 
+function cutString(str: string): number[] {
+  return _.chunk(str.split(""), str.length / 2).map((n: string[]) =>
+    Number(n.join(""))
+  );
+}
+
 function blink(stones: StoneList<number>, n: number): number {
   while (n--) {
     const newStones = new StoneList<number>();
 
-    for (const [stone, count] of stones.getAll()) {
+    for (const [stone, count] of stones.map) {
+      const str = stone.toString();
       if (stone === 0) {
         newStones.add(1, count);
+      } else if (str.length % 2 === 0) {
+        const [left, right] = cutString(str);
+        newStones.add(left, count);
+        newStones.add(right, count);
       } else {
-        const str = stone.toString();
-        if (str.length % 2 === 0) {
-          const [left, right] = _.chunk(str.split(""), str.length / 2).map(
-            (n: string[]) => Number(n.join(""))
-          );
-          newStones.add(left, count);
-          newStones.add(right, count);
-        } else {
-          newStones.add(stone * 2024, count);
-        }
+        newStones.add(stone * 2024, count);
       }
     }
     stones = newStones;
