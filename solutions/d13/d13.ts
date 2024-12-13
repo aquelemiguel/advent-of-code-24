6683a4180826bd210526969fa77710239987c44a
Miguel Mano <aquelemiguel@gmail.com>
2024-12-13 18:57:25 +0000

    🎄

--- solutions/d13/d13.ts
+++ solutions/d13/d13.ts
@@ -2,7 +2,7 @@ function parse(input: string) {
   return input.split("\n\n").map((m) => m.match(/\d+/g)!.map(Number));
 }
 
-function token([a1, a2, b1, b2, c1, c2]: number[]) {
+function tokens([a1, a2, b1, b2, c1, c2]: number[]) {
   const d = a1 * b2 - a2 * b1;
   const [a, b] = [(c1 * b2 - c2 * b1) / d, (a1 * c2 - a2 * c1) / d];
   return Number.isInteger(a) && Number.isInteger(b) ? a * 3 + b : 0;
@@ -11,7 +11,7 @@ function token([a1, a2, b1, b2, c1, c2]: number[]) {
 function solve(machines: number[][], correct: boolean) {
   return machines.reduce((acc, machine) => {
     [4, 5].forEach((i) => correct && (machine[i] += Math.pow(10, 13)));
-    return acc + token(machine);
+    return acc + tokens(machine);
   }, 0);
 }
 
