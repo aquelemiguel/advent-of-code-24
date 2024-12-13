0ff5c31cf307ae8cd1ab97e75bc3ddbea8d60c7a
Miguel Mano <aquelemiguel@gmail.com>
2024-12-13 18:38:42 +0000

    🎄

--- solutions/d13/d13.ts
+++ solutions/d13/d13.ts
@@ -2,34 +2,20 @@ function parse(input: string) {
   return input.split("\n\n").map((m) => m.match(/\d+/g)!.map(Number));
 }
 
-function token([a1, a2, b1, b2, c1, c2]: number[]): number[] {
+function token([a1, a2, b1, b2, c1, c2]: number[]) {
   const d = a1 * b2 - a2 * b1;
-  const x = (c1 * b2 - c2 * b1) / d;
-  const y = (a1 * c2 - a2 * c1) / d;
-  return [x, y];
+  const a = (c1 * b2 - c2 * b1) / d;
+  const b = (a1 * c2 - a2 * c1) / d;
+  return Number.isInteger(a) && Number.isInteger(b) ? a * 3 + b : 0;
 }
 
-function solve(machines: number[][]): number {
-  console.log("machines", machines);
-  let [a, b] = [0, 0];
-  machines.forEach((machine) => {
-    const [x, y] = token(machine);
-    console.log("x", x, "y", y);
-    if (Number.isInteger(x) && Number.isInteger(y)) {
-      [a, b] = [a + x, b + y];
-      console.log(x, y);
-    }
-  });
-  return a * 3 + b;
+function solve(machines: number[][], correct: boolean) {
+  return machines.reduce((acc, machine) => {
+    machine[4] += correct ? Math.pow(10, 13) : 0;
+    machine[5] += correct ? Math.pow(10, 13) : 0;
+    return acc + token(machine);
+  }, 0);
 }
 
-function correct(machines: number[][]) {
-  machines.forEach((m) => {
-    m[4] = m[4] + Math.pow(10, 12);
-    m[5] = m[5] + Math.pow(10, 12);
-  });
-  return machines;
-}
-
-export const p1 = (input: string): number => solve(parse(input));
-export const p2 = (input: string): number => solve(correct(parse(input)));
+export const p1 = (input: string): number => solve(parse(input), false);
+export const p2 = (input: string): number => solve(parse(input), true);
